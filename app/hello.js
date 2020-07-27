5df223c066f0d4461b1b389b3c60ba1748ad8233
Lars Thorup <lars@zealake.com>
2020-07-27 20:15:30 +0200

    prettier: improved react element formatting

--- app/hello.js
+++ app/hello.js
@@ -7,15 +7,18 @@ const { createElement: h, useState } = React;
 /** @type {React.FC<{name: string}>} */
 const Hello = ({ name }) => {
   const [value, setValue] = useState('');
+
   /** @type {React.ChangeEventHandler<HTMLInputElement>} */
   const onChange = (ev) => {
     setValue(ev.target.value);
   };
-  const h1 = h('h1', null, 'Greeting');
-  const p = h('p', null, `Hello ${value ? `${value} ` : ''}from ${name}`);
-  const input = h('label', {}, 'Enter name:', h('input', { onChange, value }));
-  const button = h('button', {}, 'Submit');
-  return h('div', { id: 'hello' }, h1, p, input, button);
+
+  return h('div', { id: 'hello' }, [
+    h('h1', null, 'Greeting'),
+    h('p', null, `Hello ${value ? `${value} ` : ''}from ${name}`),
+    h('label', {}, ['Enter name:', h('input', { onChange, value })]),
+    h('button', {}, 'Submit'),
+  ]);
 };
 
 export default Hello;

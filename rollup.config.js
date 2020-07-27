220a7e6c5a92ecc4cf6fe94fb26945d85550de2e
Lars Thorup <lars@zealake.com>
2020-07-27 21:23:02 +0200

    rollup: use react production build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,24 @@
+import alias from '@rollup/plugin-alias';
+
 export default {
   context: 'window', // Note: needed because we import React as UMD
   input: 'app/index.js',
   output: {
     file: 'build/app/index.js',
   },
+  plugins: [
+    alias({
+      entries: [
+        {
+          find: '../node_modules/react/umd/react.development.js',
+          replacement: '../node_modules/react/umd/react.production.min.js',
+        },
+        {
+          find: '../node_modules/react-dom/umd/react-dom.development.js',
+          replacement:
+            '../node_modules/react-dom/umd/react-dom.production.min.js',
+        },
+      ],
+    }),
+  ],
 };

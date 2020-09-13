8a673b7db5ba2fa425386e35f49cb3f9871fa37a
Lars Thorup <lars@zealake.com>
2020-09-13 14:03:27 +0200

    puppeteer

---
+++ dev/puppeteer-runner.js
@@ -0,0 +1,116 @@
+import * as fs from 'fs';
+import * as util from 'util';
+import ChromeLauncher from 'chrome-launcher';
+import httpServer from 'http-server';
+import puppeteer from 'puppeteer-core';
+import puppeteerToIstanbul from 'puppeteer-to-istanbul';
+import { getMochaResult, initMocha } from './browser/mocha-bridge.js';
+
+// Note: inspired by https://github.com/direct-adv-interfaces/mocha-headless-chrome/
+
+const root = '.';
+const port = 8081;
+const url = `http://127.0.0.1:${port}/dev/mocha-runner.html`;
+const reporter = 'dot';
+const timeout = 60000;
+
+const main = async () => {
+  try {
+    await startingServer();
+    const result = await runningMochaInPuppeteer();
+    await writingResult(result);
+    process.exit(result.mochaResult.result.stats.failures > 0 ? 1 : 0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+const startingServer = async () => {
+  const server = httpServer.createServer({ root });
+  await new Promise((resolve) => server.listen(port, 'localhost', resolve));
+};
+
+const runningMochaInPuppeteer = async () => {
+  const chromePaths = ChromeLauncher.Launcher.getInstallations();
+  const [chromePath] = chromePaths;
+  const options = {
+    executablePath: chromePath,
+  };
+  const browser = await puppeteer.launch(options);
+  const pages = await browser.pages();
+  const page = pages.pop();
+  page.on('console', mochaConsoleHandler);
+  page.on('pageerror', ({ message }) => {
+    console.error(message);
+  });
+  page.on('response', (/*response*/) => {
+    // console.log(`${response.status()} ${response.url()}`);
+  });
+  page.on('requestfailed', (request) => {
+    console.error(`${request.failure().errorText} ${request.url()}`);
+  });
+  page.on('dialog', (dialog) => dialog.dismiss());
+  await page.evaluateOnNewDocument(initMocha, reporter);
+  await page.coverage.startJSCoverage();
+  await page.coverage.startCSSCoverage();
+  await page.goto(url);
+  await page.waitForFunction(getMochaResult, {
+    timeout,
+  });
+  const mochaResult = await page.evaluate(getMochaResult);
+  const [jsCoverage, cssCoverage] = await Promise.all([
+    page.coverage.stopJSCoverage(),
+    page.coverage.stopCSSCoverage(),
+  ]);
+  await browser.close();
+  return {
+    mochaResult,
+    jsCoverage,
+    cssCoverage,
+  };
+};
+
+const writingResult = (result) => {
+  fs.mkdirSync('output/test', { recursive: true });
+  fs.writeFileSync(
+    'output/test/mocha-test-result.json',
+    JSON.stringify(result.mochaResult, null, 2)
+  );
+  const coverage = [...result.jsCoverage, ...result.cssCoverage];
+  let totalBytes = 0;
+  let usedBytes = 0;
+  for (let { ranges, text, url } of coverage) {
+    if (
+      !url.includes('/node_modules/') &&
+      (url.endsWith('.js') || url.endsWith('.css')) &&
+      !url.endsWith('.test.js')
+    ) {
+      totalBytes += text ? text.length : 0;
+      for (let range of ranges) {
+        // console.log(range.start, range.end);
+        usedBytes += range.end - range.start - 1;
+      }
+    }
+  }
+  console.log(`Code coverage in bytes: ${(usedBytes / totalBytes) * 100}%`);
+  puppeteerToIstanbul.write(coverage, {
+    includeHostname: true,
+    storagePath: './.nyc_output',
+  });
+};
+
+const mochaConsoleHandler = (msg) => {
+  const args = msg._args;
+  Promise.all(args.map((a) => a.jsonValue().catch(() => ''))).then((args) => {
+    // process stdout stub
+    let isStdout = args[0] === 'stdout:';
+    isStdout && (args = args.slice(1));
+
+    let msg = util.format(...args);
+    !isStdout && msg && (msg += '\n');
+    process.stdout.write(msg);
+  });
+};
+
+main();
